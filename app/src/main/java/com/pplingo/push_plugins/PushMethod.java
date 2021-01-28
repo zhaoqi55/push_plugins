187a4f1b596052338b63e0b744f4e8f10ae4d18f
zhaoqi <550655294@qq.com>
2021-01-28 15:44:14 +0800

    Initial commit

---
+++ app/src/main/java/com/pplingo/push_plugins/PushMethod.java
@@ -0,0 +1,18 @@
+package com.pplingo.push_plugins;
+
+import cn.jpush.android.api.JPushInterface;
+
+
+///推送method
+public class PushMethod {
+
+
+
+    static  void  setAlias(){
+
+    }
+
+
+
+
+}
